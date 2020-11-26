19e970ffdbdfd7530b6ecd0517a3e8701d06cd2e
safta.antoneta <safta.antoneta@gmail.com>
2020-11-26 21:21:22 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,31 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import static com.example.demo.api.PersonController.API_PERSON;
+
+@RequestMapping(API_PERSON)
+@RestController
+public class PersonController {
+
+    public static final String API_PERSON= "/api/v1/person";
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+}
